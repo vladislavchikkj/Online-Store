8232ce47ecf0227a305c51960e46904c5d588802
Vlad <vlad.kosevichhhh@gmail.com>
2022-12-23 15:23:18 +0300

    fix: position catalog item

--- online-store/src/pages/catalog-page/index.ts
+++ online-store/src/pages/catalog-page/index.ts
@@ -1,17 +1,19 @@
 import  Page from '../core/templates/page';
+import Products from '../core/components/products';
 
 class CatalogPage extends Page {
-    static TextObgect = {
-        MainTitle: 'Catalog Page',
 
-    };
+    private products: Products;
+
 
     constructor(id: string) {
         super(id);
+        this.products = new Products('products');
     }
-
+    
     render() {
-        const title = this.createPage(CatalogPage.TextObgect.MainTitle)
+        const title = this.createPage('');
+        title.append(this.products.render())
         this.container.append(title);
         return this.container;
     }
